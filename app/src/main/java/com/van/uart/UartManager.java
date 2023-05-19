8df9ce7eef8baf3d2404117e8749ff8281761b96
h4de5ing <lex109@qq.com>
2023-05-19 17:14:09 +0800

    增加串口库

---
+++ app/src/main/java/com/van/uart/UartManager.java
@@ -0,0 +1,126 @@
+package com.van.uart;
+
+public class UartManager {
+
+    static {
+        System.loadLibrary("VanUart");
+    }
+
+    public static enum BaudRate {
+        B1200, B2400, B4800, B9600, B19200, B38400, B57600, B115200, B230400;
+    }
+
+    private int id;
+    private String name;
+    private BaudRate baudRate;
+
+    public UartManager() {
+        id = -1;
+        name = "";
+        baudRate = BaudRate.B115200;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BaudRate getBaudRate() {
+        return baudRate;
+    }
+
+    public static BaudRate getBaudRate(int baudrate) {
+        BaudRate value = null;
+        switch (baudrate) {
+            case 9600:
+                value = BaudRate.B9600;
+                break;
+            case 19200:
+                value = BaudRate.B19200;
+                break;
+            case 57600:
+                value = BaudRate.B57600;
+                break;
+            case 115200:
+                value = BaudRate.B115200;
+                break;
+            case 230400:
+                value = BaudRate.B230400;
+                break;
+        }
+        return value;
+    }
+
+    public void open(String name, BaudRate baudRate) throws LastError {
+        int ids = open(name, baudRate.ordinal());
+        System.out.println("打开结果：" + ids);
+        this.id = ids;
+        this.name = name;
+        this.baudRate = baudRate;
+    }
+
+    public boolean open2(String name, BaudRate baudRate) throws LastError {
+        int ids = open(name, baudRate.ordinal());
+        System.out.println("打开结果：" + ids);
+        this.id = ids;
+        this.name = name;
+        this.baudRate = baudRate;
+        return ids == 0;
+    }
+
+    public void close() {
+        if (-1 != id) {
+            close(id);
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isOpened() {
+        return id == 0;
+    }
+
+    public boolean isOpen() {
+        if (-1 != id) {
+            return isOpen(id);
+        }
+
+        return false;
+    }
+
+    public int write(final byte[] data, int size) throws LastError {
+        if (-1 != id) {
+            return write(id, data, size);
+        }
+
+        return -1;
+    }
+
+    public int read(byte[] buf, int size, int wait, int interval) throws LastError {
+        if (-1 != id) {
+            return read(id, buf, size, wait, interval);
+        }
+        return -1;
+    }
+
+    public void stopRead() {
+        if (-1 != id) {
+            stopRead(id);
+        }
+    }
+
+    public static native String[] devices();
+
+    private native int open(String name, int baudRate) throws LastError;
+
+    private native void close(int id);
+
+    private native boolean isOpen(int id);
+
+    private native int write(int id, final byte[] data, int size) throws LastError;
+
+    private native int read(int id, byte[] buf, int size, int wait, int interval) throws LastError;
+
+    private native void stopRead(int id);
+}
